0b81e1397e07ce253003d0d9384b5b9b324b475f
shm_usuf <shmusuf@gmail.com>
2023-02-17 23:29:11 +0600

    cartesian product added

---
+++ cartesian-product.js
@@ -0,0 +1,20 @@
+// A
+// B
+// A X B
+// A = [1, 2]
+// B = [x, y]
+// A X B = [[1, x],[1,y],[2, x],[2,y]]
+
+function cartesianProduct(setA, setB) {
+  const result = [];
+
+  for (let i = 0; i < setA.length; i++) {
+    for (let j = 0; j < setB.length; j++) {
+      result.push([setA[i], setB[j]]);
+    }
+  }
+
+  return result;
+}
+
+console.log(cartesianProduct([1, 2], ["x", "y"]));
